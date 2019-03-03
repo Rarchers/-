41a21de165efaa6a3d878c31711d15604e471440
Rarchers <2553263392@qq.com>
2019-03-03 19:03:38 +0800

    先...这样??

--- app/src/main/java/com/rarcher/Fragments/Online.java
+++ app/src/main/java/com/rarcher/Fragments/Online.java
@@ -1,19 +1,31 @@
 package com.rarcher.Fragments;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.rarcher.DataBase.LocalDB;
+import com.rarcher.DataBase.Nowusers;
 import com.rarcher.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Online extends Fragment {
+public class Online extends Fragment implements View.OnClickListener {
 
+
+    private GridView gv;
+    private ListView listView;
+    private TextView title;
+    private String[] home = {"衣","食","住","行","娱"};
 
     public Online() {
         // Required empty public constructor
@@ -27,4 +39,23 @@ public class Online extends Fragment {
         return inflater.inflate(R.layout.fragment_online, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+    }
+
+    private void init(View view){
+        listView = view.findViewById(R.id.lv_menu);
+        gv = view.findViewById(R.id.grid);
+        title = view.findViewById(R.id.tv_titile);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,home);
+        listView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
